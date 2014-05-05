fde2a9f1a23764cd609e886c49f71a4757b2641a
Bill <whilke@gmail.com>
2014-05-04 21:10:29 -0700

    Updated DB

--- db/spl.sql
+++ db/spl.sql
@@ -66,6 +66,117 @@ LOCK TABLES `login_attempts` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `matches`
+--
+
+DROP TABLE IF EXISTS `matches`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `matches` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `season_id` int(11) NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `gamedate` datetime DEFAULT NULL,
+  `proposeddate` datetime DEFAULT NULL,
+  `home_team_id` int(11) NOT NULL,
+  `away_team_id` int(11) NOT NULL,
+  `home_team_state_id` int(11) DEFAULT NULL,
+  `home_team_points` int(11) NOT NULL DEFAULT '0',
+  `away_team_points` varchar(45) NOT NULL DEFAULT '0',
+  `away_team_state_id` int(11) DEFAULT NULL,
+  `active` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `matches`
+--
+
+LOCK TABLES `matches` WRITE;
+/*!40000 ALTER TABLE `matches` DISABLE KEYS */;
+/*!40000 ALTER TABLE `matches` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `seasons`
+--
+
+DROP TABLE IF EXISTS `seasons`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `seasons` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `start` datetime DEFAULT NULL,
+  `end` datetime DEFAULT NULL,
+  `active` int(11) NOT NULL DEFAULT '0',
+  `tag` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `seasons`
+--
+
+LOCK TABLES `seasons` WRITE;
+/*!40000 ALTER TABLE `seasons` DISABLE KEYS */;
+INSERT INTO `seasons` VALUES (1,'sdfsdf','2014-05-02 00:00:00','2014-05-21 00:00:00',1,'S1'),(2,'Season 2','0000-00-00 00:00:00','0000-00-00 00:00:00',1,'S2'),(3,'Test','2014-05-02 00:00:00','2014-05-03 00:00:00',1,'T1'),(4,'Season 1','2014-05-01 00:00:00','2014-05-31 00:00:00',1,'S1'),(5,'Test','2014-05-06 00:00:00','2014-05-06 00:00:00',1,'S232');
+/*!40000 ALTER TABLE `seasons` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `seasons_teams`
+--
+
+DROP TABLE IF EXISTS `seasons_teams`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `seasons_teams` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `season_id` int(11) NOT NULL,
+  `team_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `seasons_teams`
+--
+
+LOCK TABLES `seasons_teams` WRITE;
+/*!40000 ALTER TABLE `seasons_teams` DISABLE KEYS */;
+INSERT INTO `seasons_teams` VALUES (4,4,1);
+/*!40000 ALTER TABLE `seasons_teams` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `states`
+--
+
+DROP TABLE IF EXISTS `states`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `states` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `desc` varchar(45) NOT NULL,
+  `code` varchar(5) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `states`
+--
+
+LOCK TABLES `states` WRITE;
+/*!40000 ALTER TABLE `states` DISABLE KEYS */;
+INSERT INTO `states` VALUES (1,'Win','W'),(2,'Loss','L'),(3,'Forfeit','FF'),(4,'Noshow','NS');
+/*!40000 ALTER TABLE `states` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `teams`
 --
 
@@ -89,7 +200,7 @@ CREATE TABLE `teams` (
   `region` varchar(15) DEFAULT NULL,
   `teamscol` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -98,7 +209,7 @@ CREATE TABLE `teams` (
 
 LOCK TABLES `teams` WRITE;
 /*!40000 ALTER TABLE `teams` DISABLE KEYS */;
-INSERT INTO `teams` VALUES (1,3,'Test','None','test3fff','','','','','','',NULL,'email@email.com','USE',NULL);
+INSERT INTO `teams` VALUES (1,3,'Test','None','test3fff','etest','test','test','','','',NULL,'email@email.com','USE',NULL),(2,4,'STRIFE Pro League ADMINS','testing','','','','','','','',NULL,'sdfsdf','USE',NULL),(3,5,'Test',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(4,6,'Test2',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(5,7,'My Team',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(6,8,'dfldkjf',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(7,9,'Test my team',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(8,10,'Test my team2',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(9,11,'Test2334',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(10,12,'blah',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(11,13,'blsh2',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(12,14,'My Cool Team',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
 /*!40000 ALTER TABLE `teams` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -124,7 +235,7 @@ CREATE TABLE `users` (
   `last_login` int(11) unsigned DEFAULT NULL,
   `active` tinyint(1) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -133,7 +244,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'127.0.0.1','Admin','4edd97432e02401b4429478c974f2345db4bda1c','','admin@strifeproleague.com',NULL,NULL,NULL,'9e004f3af2cca27b9494fef8e2cffb80d5487a86',1268889823,1398354790,1),(3,'\0\0','Test','773958800567a8ca3813b51c50205aa51052b920',NULL,'bill.hilke@gmail.com',NULL,NULL,NULL,NULL,1398308599,1398363525,1);
+INSERT INTO `users` VALUES (1,'127.0.0.1','Admin','4edd97432e02401b4429478c974f2345db4bda1c','','admin@strifeproleague.com',NULL,NULL,NULL,'9e004f3af2cca27b9494fef8e2cffb80d5487a86',1268889823,1399161641,1),(3,'\0\0','Test','773958800567a8ca3813b51c50205aa51052b920',NULL,'bill.hilke@gmail.com',NULL,NULL,NULL,NULL,1398308599,1398397290,1),(4,'\0\0','STRIFE Pro League ADMINS','78be72ea148807cfe2bd9a89fbab8db16bab3cfd',NULL,'test@test.com',NULL,NULL,NULL,NULL,1398371749,1398938549,1),(5,'\0\0','Test1','746ec60826e22e8e5c8c37edfda84e3565d70358',NULL,'test2@test.com','cbd897512dd8d99a0035619419adc6617db19fbb',NULL,NULL,NULL,1398373843,1398373843,0),(6,'\0\0','Test2','be0f33608466c7d0e1209819d085eba881cdd7d9',NULL,'test3@test.com','b8ca81c1132d1137c4b8d475d98a0eb8ec64de53',NULL,NULL,NULL,1398374150,1398374150,0),(7,'\0\0','My Team','0b7fd488fed6c7d3efe8058cc01af25aeca7dbdb',NULL,'whilke@g33mail.com','1287b8dac3e21ce2b3dfd96ccd8a6b1c9bc3d879',NULL,NULL,NULL,1398385831,1398385831,0),(8,'\0\0','dfldkjf','0db9184140b2ef9c8564efc1b697990b73c6ae71',NULL,'test55@test.com','0a09b39c6d45c575fd332e65404fc01b45085726',NULL,NULL,NULL,1398385980,1398385980,0),(9,'\0\0','Test my team','f367851085dc67eb6baf12ac4e428a6191543aff',NULL,'test333@test.com','4b6f2cbc48fc68391fecc64ed443eda32ded707f',NULL,NULL,NULL,1398386054,1398386054,0),(10,'\0\0','Test my team2','929761acdabc46be6ccee9285cc50ae8f8396b67',NULL,'test3ee33@test.com','1bf9120b4dcab8cbd84123ff5b2f8cec4d5a32fd',NULL,NULL,NULL,1398386097,1398386097,0),(11,'\0\0','Test2334','178bb9cfae584079f326628cafbc88bd043d12bd',NULL,'test5555@test.com','01afb458c6a36997dd015b2ce16b26908c8d0263',NULL,NULL,NULL,1398386401,1398386401,0),(12,'\0\0','blah','82c5f85c273eac99596ce1444896e1866434ef84',NULL,'blah@test.com','d34f6abf69e3cda06739a3869f53c0eac448852f',NULL,NULL,NULL,1398386545,1398386545,0),(13,'\0\0','blsh2','7b78dfc6a1f11353bf836e4bd5a13fcc7e4cfbcd',NULL,'blah2@test.com',NULL,NULL,NULL,NULL,1398386898,1398386898,1),(14,'\0\0','My Cool Team','a0e709beb254f7c478f029e537bdfd47c3f4ee70',NULL,'whilke@gmail.com',NULL,NULL,NULL,NULL,1398387785,1398387785,1);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -154,7 +265,7 @@ CREATE TABLE `users_groups` (
   KEY `fk_users_groups_groups1_idx` (`group_id`),
   CONSTRAINT `fk_users_groups_groups1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
   CONSTRAINT `fk_users_groups_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -163,8 +274,35 @@ CREATE TABLE `users_groups` (
 
 LOCK TABLES `users_groups` WRITE;
 /*!40000 ALTER TABLE `users_groups` DISABLE KEYS */;
-INSERT INTO `users_groups` VALUES (1,1,1),(2,1,2),(4,3,2);
+INSERT INTO `users_groups` VALUES (1,1,1),(2,1,2),(4,3,2),(5,4,2),(6,5,2),(7,6,2),(8,7,2),(9,8,2),(10,9,2),(11,10,2),(12,11,2),(13,12,2),(14,13,2),(15,14,2);
 /*!40000 ALTER TABLE `users_groups` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `weeks`
+--
+
+DROP TABLE IF EXISTS `weeks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `weeks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tag` varchar(45) NOT NULL,
+  `start` datetime NOT NULL,
+  `end` datetime NOT NULL,
+  `season_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `weeks`
+--
+
+LOCK TABLES `weeks` WRITE;
+/*!40000 ALTER TABLE `weeks` DISABLE KEYS */;
+INSERT INTO `weeks` VALUES (2,'W1','2014-05-12 00:00:00','2014-05-18 00:00:00',4);
+/*!40000 ALTER TABLE `weeks` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -176,4 +314,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-04-24 11:40:36
+-- Dump completed on 2014-05-04 21:09:32

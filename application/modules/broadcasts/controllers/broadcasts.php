6920ffb7334fea813412768107273a7849062efb
Bill <whilke@gmail.com>
2014-09-23 07:00:40 -0700

    time fixes

--- application/modules/broadcasts/controllers/broadcasts.php
+++ application/modules/broadcasts/controllers/broadcasts.php
@@ -34,7 +34,8 @@ class Broadcasts extends MY_Controller
                 }
                 else
                 {
-                    $cast->timestamp = $cast->timestamp . " GMT";
+                    $oDate = new DateTime($cast->timestamp);
+                    $cast->timestamp = $oDate->format('m/d/Y H:i:s') ." GMT";
                 }
                 
                 date_default_timezone_set('GMT');
